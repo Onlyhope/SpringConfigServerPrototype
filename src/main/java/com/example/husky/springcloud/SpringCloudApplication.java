7435ced73b850a254b55831a1b1975a690f6188e
Aaron Lee <leeaaron326@gmail.com>
2019-04-11 14:28:22 -0400

    adding changes

--- src/main/java/com/example/husky/springcloud/SpringCloudApplication.java
+++ src/main/java/com/example/husky/springcloud/SpringCloudApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer
-@Configuration
 public class SpringCloudApplication {
 
 	public static void main(String[] args) {
