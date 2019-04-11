3ae32cbd91f7b04191dfb025df9d20cffde896ff
Aaron Lee <leeaaron326@gmail.com>
2019-04-11 13:57:20 -0400

    Initial commit

---
+++ src/main/java/com/example/husky/springcloud/SpringCloudApplication.java
@@ -0,0 +1,17 @@
+package com.example.husky.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableConfigServer
+@Configuration
+public class SpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApplication.class, args);
+	}
+
+}
